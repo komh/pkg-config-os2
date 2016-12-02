1251bdd1dd1cb30cb274a137bb29947b30e830d7
KO Myung-Hun <komh@chollian.net>
2016-12-02 10:21:04 +0900

    my: build: specify system default paths for OS/2
    By this /@unixroot prefix is supported.
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -1,3 +1,7 @@
 extproc sh
 
-./configure --prefix=/usr --with-internal-glib "$@"
+./configure --prefix=/usr \
+            --with-internal-glib \
+            --with-system-include-path="/@unixroot/usr/include;/usr/include" \
+            --with-system-library-path="/@unixroot/usr/lib;/@unixroot/lib;/usr/lib;/lib" \
+            "$@"
