3e7cff59a2f8dd96884f9b262358abf5732752e1
KO Myung-Hun <komh@chollian.net>
2017-01-12 13:25:25 +0900

    my: use default prefix
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -1,7 +1,6 @@
 extproc sh
 
-./configure --prefix=/usr \
-            --with-internal-glib \
+./configure --with-internal-glib \
             --with-system-include-path="/@unixroot/usr/include;/usr/include" \
             --with-system-library-path="/@unixroot/usr/lib;/@unixroot/lib;/usr/lib;/lib" \
             "$@"
