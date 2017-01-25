0d4d1469ab0cd2e8545f2a95a3a7d7b8b1151384
KO Myung-Hun <komh@chollian.net>
2017-01-25 14:59:28 +0900

    my: add --disable-compiler-warnings
    This fixes compilation on gcc4. Some warnings are treated as errors.
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -3,4 +3,5 @@ extproc sh
 ./configure --with-internal-glib \
             --with-system-include-path="/@unixroot/usr/include;/usr/include" \
             --with-system-library-path="/@unixroot/usr/lib;/@unixroot/lib;/usr/lib;/lib" \
+            --disable-compile-warnings \
             "$@"
